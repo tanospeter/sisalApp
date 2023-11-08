e79a3588195d420431564337221d06fcdf43af38
ebanyai <evelin.banyai@gmail.com>
2023-11-08 10:31:44 +0100

    added api route prefix
    prefix can be defined with API_ROUTE_PREFIX in .env

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,16 @@ const express = require('express')
 const cors = require("cors")
 
 const app = express()
+const prefix = process.env.API_ROUTE_PREFIX  || ""
 
 app.use(cors())
 
 app.use(express.json()) 
 
-app.use("/api/getentitymeta", require("./routes/getEntity"));
-app.use("/api/getdatinginfo", require("./routes/getDatingInfo"));
-app.use("/api/getsisalchrono", require("./routes/getSisalChrono"));
-app.use("/api/getAdvancedRes", require("./routes/getAdvancedRes"));
+app.use(prefix+"/api/getentitymeta", require("./routes/getEntity"));
+app.use(prefix+"/api/getdatinginfo", require("./routes/getDatingInfo"));
+app.use(prefix+"/api/getsisalchrono", require("./routes/getSisalChrono"));
+app.use(prefix+"/api/getAdvancedRes", require("./routes/getAdvancedRes"));
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
